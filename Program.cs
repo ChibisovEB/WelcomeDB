878fe94f5eba456f3c59de7baaa70c7d08cecd75
Landgraf <e4ibisoff@ya.ru>
2023-05-04 11:49:45 +0700

    First commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿Console.WriteLine("Готов к труду и обороне!");
+﻿Console.WriteLine("Поехали!");
+Console.WriteLine("Введите массив строк. Для завершения ввода введите пустую строку.");
+Console.Write("");
+string s="1234";
+List<string> lst=new List<string>();
+lst.Add(s);
+Console.WriteLine(lst[0].Length);
+Console.WriteLine(lst.LastIndexOf);
 
+foreach (string str in arr)
+{
+    str.Substring(35, 5); /*или*/ str.IndexOf("__0.00")
+}
